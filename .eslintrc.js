703d72bc68b3e618ac6708a6e1ad15b711ecc5d9
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-01-15 16:15:35 -0300

    feat: added ESLint and AirBnB style guide

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+
+  env: {
+    browser: true
+  },
+
+  extends: [
+    'airbnb-base'
+  ],
+
+  globals: {
+    'process': true,
+    'chrome': true
+  },
+
+  rules: {
+    'no-param-reassign': 'off',
+
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
